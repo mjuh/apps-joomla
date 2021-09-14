23a897a50dacb03850bfb3c7a20cd6f4a8f333b6
Alexander Rezvov <rezvov@majordomo.ru>
2021-09-14 15:47:24 +0300

    flake.nix: add deploy

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "Docker container with Joomla installer";
   inputs = {
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+    majordomo.url = "git+https://gitlab.intr/_ci/nixpkgs?ref=deploy_postfix";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... } @ inputs: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, majordomo, ... } @ inputs: 
   let
     system = "x86_64-linux";
   in {
@@ -35,6 +36,18 @@
       joomla-4-0 = import ./container.nix {
         inherit nixpkgs system;
         joomla_version = "4.0.3";
+      };
+
+      deploy-3-9 = majordomo.outputs.deploy {
+        tag = "apps/joomla";
+        pkg_name = "joomla-3-9";
+        postfix = "_3_9";
+      };
+
+      deploy-4-0 = majordomo.outputs.deploy {
+        tag = "apps/joomla";
+        pkg_name = "joomla-4-0";
+        postfix = "_4_0";
       };
     };
 

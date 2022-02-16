1167a0ecc910f5f451b32356dcc6d5470e912c6f
Зелинский Александр <zelinskiy@majordomo.ru>
2022-02-16 22:56:04 +0300

    Update Dockerfile and install.py 4.1.0 WIP

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine:edge
 
 ENV PHANTOMJS_ARCHIVE="phantomjs.tar.gz"
+ENV JOOMLA_INSTALLATION_DISABLE_LOCALHOST_CHECK=1
 
 RUN echo '@edge http://nl.alpinelinux.org/alpine/edge/main'>> /etc/apk/repositories && \
- apk --update add curl python3 && \
+ apk --update add curl python3 python3-dev py3-pip gcc musl-dev libffi-dev && \
  curl -Lk -o $PHANTOMJS_ARCHIVE https://github.com/fgrehm/docker-phantomjs2/releases/download/v2.0.0-20150722/dockerized-phantomjs.tar.gz && \
  tar -xf $PHANTOMJS_ARCHIVE -C /tmp/ && \
  cp -R /tmp/etc/fonts /etc/ && \
@@ -14,9 +15,9 @@ RUN echo '@edge http://nl.alpinelinux.org/alpine/edge/main'>> /etc/apk/repositor
  cp -R /tmp/usr/share/* /usr/share/ && \
  cp /tmp/usr/local/bin/phantomjs /usr/bin/ && \
  rm -rf $PHANTOMJS_ARCHIVE /tmp/* && \
- pip3 install selenium
+ pip3 install selenium==3.8.0
 COPY dist /dist
-RUN tar czf joomla-3.9.13.tgz -C /dist . && chmod o+r joomla-3.9.13.tgz
+RUN tar czf joomla-4.1.0.tgz -C /dist . && chmod o+r joomla-4.1.0.tgz
 
 COPY install.py /install
 WORKDIR /workdir
--- install.py
+++ install.py
@@ -11,7 +11,7 @@ import re
 import shutil
 import time
 
-VERSION = "3.9.13"
+VERSION = "4.1.0"
 ARCHIVE_PATH = "/joomla-{}.tgz".format(VERSION)
 INSTALLER_URL = "http://{}/installation/index.php".format(os.environ["DOMAIN_NAME"])
 APP_LOCALE = os.environ.get("APP_LOCALE") or "ru-RU"
@@ -27,10 +27,8 @@ DB_USER = os.environ["DB_USER"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_NAME = os.environ["DB_NAME"]
 # xpath could be obtained from web browser's development tools
-XPATHS = {"database_error":     '//*[@id="system-message-container"]/div[2]/div',
-          "next_button":        '//*[@id="adminForm"]/div[3]/div/div/a',
-          "install_button":     '//*[@id="adminForm"]/div[7]/div/div/a[2]',
-          "remove_inst_folder": '//*[@id="adminForm"]/div[3]/input'}
+XPATHS = {"lang_table": "/html/body/div/div/div/div/main/div/div[3]/div/fieldset[2]/div/form/table/tbody", # really fragile test before update
+          "finish": '/html/body/div/div/div/div/main/div/div[3]/div/div/div/div/button[1]'}
 
 
 def main():
@@ -43,72 +41,49 @@ def main():
         d = webdriver.PhantomJS()
         d.get(INSTALLER_URL)
 
-        print("Submitting 'Configufation' form")
+        # main configuration page
+        print("Submitting form")
         d.find_element_by_id("jform_site_name").send_keys(APP_TITLE)
-        d.find_element_by_id("jform_admin_email").send_keys(ADMIN_EMAIL)
+        d.find_element_by_id("step1").click()
+
         d.find_element_by_id("jform_admin_user").send_keys(ADMIN_USERNAME)
+        d.find_element_by_id("jform_admin_username").send_keys(ADMIN_USERNAME)
         d.find_element_by_id("jform_admin_password").send_keys(ADMIN_PASSWORD)
-        d.find_element_by_id("jform_admin_password2").send_keys(ADMIN_PASSWORD)
-        d.find_element_by_xpath(XPATHS["next_button"]).click()
+        d.find_element_by_id("jform_admin_email").send_keys(ADMIN_EMAIL)
+        d.find_element_by_id("step2").click()
 
-        XPATHS["next_button"] = '//*[@id="adminForm"]/div[9]/div/div/a[2]'
-
-        print("Submitting 'Database' form")
-        WebDriverWait(d, 60).until(EC.element_to_be_clickable((By.ID, "jform_db_name")))
         db_host_field = d.find_element_by_id("jform_db_host")
         db_host_field.clear()   # Joomla installer sets 'localhost' here by default
         db_host_field.send_keys(DB_HOST)
         d.find_element_by_id("jform_db_user").send_keys(DB_USER)
         d.find_element_by_id("jform_db_pass").send_keys(DB_PASSWORD)
-        d.find_element_by_id("jform_db_name").send_keys(DB_NAME)
-        d.find_element_by_xpath(XPATHS["next_button"]).click()
+        db_name_field = d.find_element_by_id("jform_db_name")  # Joomla installer sets 'joomla_db' here by default
+        db_name_field.clear()
+        db_name_field.send_keys(DB_NAME)
+        d.find_element_by_id("setupButton").click()
 
-        # When database host name differs from 'localhost', Joomla installer
-        # displays error message with file name like "_JoomlaheyDj35lRLgCAjsZmWoD0.txt"
-        # we need to delete this file from installation/ directory to proceed
-        print("Confirming website ownership")
-        WebDriverWait(d, 60).until(EC.presence_of_element_located((By.XPATH, XPATHS["database_error"])))
-        database_error_text = d.find_element_by_xpath(XPATHS["database_error"]).text
-        file_to_delete = re.match(".+(?P<filename>_[A-z0-9]+\.txt).+",
-                                database_error_text).groupdict().get("filename")
-        assert file_to_delete, "Failed to extract file name from message: {}".format(database_error_text)
-        print("Deleting {} file from installation dir".format(file_to_delete))
-        os.unlink("installation/{}".format(file_to_delete))
-        WebDriverWait(d, 60).until(EC.invisibility_of_element_located((By.ID, "loading-logo")))
-        d.find_element_by_xpath(XPATHS["next_button"]).click()
-        print("Try to set jform_db_pass again! ")
-        time.sleep(1)
-        WebDriverWait(d, 5).until(EC.element_to_be_clickable((By.ID, "jform_db_pass")))
-        d.find_element_by_id("jform_db_pass").click()
-        d.find_element_by_id("jform_db_pass").send_keys(DB_PASSWORD)
-        d.find_element_by_xpath(XPATHS["next_button"]).click()
-        print("Finishing installation")
-        WebDriverWait(d, 60).until(EC.visibility_of_element_located((By.ID, "jform_sample_file")))
-        d.find_element_by_xpath(XPATHS["install_button"]).click()
-
+        # page with installing additional lang
         print("Installing language pack by name: {}".format(APP_LANG_NAME))
-        WebDriverWait(d, 120).until(EC.element_to_be_clickable((By.ID, "instLangs")))
-        d.find_element_by_id("instLangs").click()
-
-        XPATHS["next_button"] = '//*[@id="adminForm"]/div[3]/div/div/a[2]'
-
-        WebDriverWait(d, 60).until(EC.visibility_of_element_located((By.ID, "defaultlanguage")))
+        WebDriverWait(d, 60).until(EC.visibility_of_element_located((By.ID, "installAddFeatures")))
+        d.find_element_by_id("installAddFeatures").click()
+        # page with lang selection
+        WebDriverWait(d, 60).until(EC.visibility_of_element_located((By.XPATH, XPATHS["lang_table"])))
         checkbox_id = next((e.get_attribute("for") for e in d.find_elements_by_tag_name("label")
-                            if APP_LANG_NAME in e.text))
+                            if APP_LANG_NAME in e.text
+                            ))
         d.find_element_by_id(checkbox_id).click()
-        d.find_element_by_xpath(XPATHS["next_button"]).click()
+        d.find_element_by_id("installLanguagesButton").click()
 
-        XPATHS["next_button"] = '//*[@id="adminForm"]/div[4]/div/div/a[2]'
+        # page with defalt lang
+        WebDriverWait(d, 60).until(EC.visibility_of_element_located((By.ID, "defaultlanguage")))
+        lang_id = next((e.get_attribute("for") for e in d.find_elements_by_tag_name("label")
+                            if APP_LANG_NAME in e.text
+                        ))[-1] # if think that we have only Russian and English then one last symbol for identification is more then enough
+        d.find_element_by_id("admin-language-cb" + lang_id).click()
+        d.find_element_by_id("site-language-cb" + lang_id).click()
+        d.find_element_by_id("defaultLanguagesButton").click()
 
-        print("Selecting default locale: {}".format(APP_LOCALE))
-        WebDriverWait(d, 60).until(EC.visibility_of_element_located((By.ID, "multilanguageOptions")))
-        for each in d.find_elements_by_tag_name("input"):
-            if each.get_attribute("value") == APP_LOCALE: each.click()
-        d.find_element_by_xpath(XPATHS["next_button"]).click()
-
-        print("Removing installation dir")
-        WebDriverWait(d, 60).until(EC.element_to_be_clickable((By.XPATH, XPATHS["remove_inst_folder"])))
-        d.find_element_by_xpath(XPATHS["remove_inst_folder"]).click()
+        d.find_element_by_xpath(XPATHS["finish"]).click()
 
         d.quit()
     except Exception as e:

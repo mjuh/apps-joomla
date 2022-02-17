c6edf2ede81ef74843b8cf0471917d2717564384
Зелинский Александр <zelinskiy@majordomo.ru>
2022-02-17 20:40:14 +0300

    Fix deprecation warrnings, add waits for button change

--- install.py
+++ install.py
@@ -45,7 +45,7 @@ def setup_joomla():
     options.add_argument("--no-sandbox") # chrome crashes otherwise
 
     d = webdriver.Chrome(options=options)
-    Wait = WebDriverWait(d, 60)
+    Wait = WebDriverWait(d, 10)
 
     try:
 
@@ -55,58 +55,57 @@ def setup_joomla():
         print("Submitting form")
         d.save_screenshot("test.png")
         Wait.until(EC.element_to_be_clickable((By.ID, "jform_site_name")))
-        d.find_element_by_id("jform_site_name").send_keys(APP_TITLE)
-        d.find_element_by_id("step1").click()
+        d.find_element(By.ID, "jform_site_name").send_keys(APP_TITLE)
+        d.find_element(By.ID, "step1").click()
 
         Wait.until(EC.element_to_be_clickable((By.ID, "jform_admin_user")))
-        d.find_element_by_id("jform_admin_user").send_keys(ADMIN_USERNAME)
-        d.find_element_by_id("jform_admin_username").send_keys(ADMIN_USERNAME)
-        d.find_element_by_id("jform_admin_password").send_keys(ADMIN_PASSWORD)
-        d.find_element_by_id("jform_admin_email").send_keys(ADMIN_EMAIL)
-        d.find_element_by_id("step2").click()
+        d.find_element(By.ID, "jform_admin_user").send_keys(ADMIN_USERNAME)
+        d.find_element(By.ID, "jform_admin_username").send_keys(ADMIN_USERNAME)
+        d.find_element(By.ID, "jform_admin_password").send_keys(ADMIN_PASSWORD)
+        d.find_element(By.ID, "jform_admin_email").send_keys(ADMIN_EMAIL)
+        Wait.until(EC.element_to_be_clickable((By.ID, "step2")))
+        d.find_element(By.ID, "step2").click()
 
         Wait.until(EC.element_to_be_clickable((By.ID, "jform_db_host")))
-        db_host_field = d.find_element_by_id("jform_db_host")
+        db_host_field = d.find_element(By.ID, "jform_db_host")
         db_host_field.clear()   # Joomla installer sets 'localhost' here by default
         db_host_field.send_keys(DB_HOST)
-        d.find_element_by_id("jform_db_user").send_keys(DB_USER)
-        d.find_element_by_id("jform_db_pass").send_keys(DB_PASSWORD)
-        db_name_field = d.find_element_by_id("jform_db_name")  # Joomla installer sets 'joomla_db' here by default
+        d.find_element(By.ID, "jform_db_user").send_keys(DB_USER)
+        d.find_element(By.ID, "jform_db_pass").send_keys(DB_PASSWORD)
+        db_name_field = d.find_element(By.ID, "jform_db_name")  # Joomla installer sets 'joomla_db' here by default
         db_name_field.clear()
         db_name_field.send_keys(DB_NAME)
-        d.find_element_by_id("setupButton").click()
+        Wait.until(EC.element_to_be_clickable((By.ID, "setupButton")))
+        d.find_element(By.ID, "setupButton").click()
 
         # page with installing additional lang
         print("Installing language pack by name: {}".format(APP_LANG_NAME))
         Wait.until(EC.visibility_of_element_located((By.ID, "installAddFeatures")))
-        d.find_element_by_id("installAddFeatures").click()
+        d.find_element(By.ID, "installAddFeatures").click()
         # page with lang selection
         Wait.until(EC.visibility_of_element_located((By.XPATH, XPATHS["lang_table"])))
-        checkbox_id = next((e.get_attribute("for") for e in d.find_elements_by_tag_name("label")
+        checkbox_id = next((e.get_attribute("for") for e in d.find_elements(By.TAG_NAME, "label")
                             if APP_LANG_NAME in e.text
                             ))
-        d.find_element_by_id(checkbox_id).click()
-        d.find_element_by_id("installLanguagesButton").click()
+        d.find_element(By.ID, checkbox_id).click()
+        d.find_element(By.ID, "installLanguagesButton").click()
 
         # page with default lang
         Wait.until(EC.visibility_of_element_located((By.ID, "defaultlanguage")))
-        lang_id = next((e.get_attribute("for") for e in d.find_elements_by_tag_name("label")
+        lang_id = next((e.get_attribute("for") for e in d.find_elements(By.TAG_NAME, "label")
                             if APP_LANG_NAME in e.text
                         ))[-1] # if think that we have only Russian and English, then one last symbol for identification is more then enough
-        d.find_element_by_id("admin-language-cb" + lang_id).click()
-        d.find_element_by_id("site-language-cb" + lang_id).click()
-        d.find_element_by_id("defaultLanguagesButton").click()
+        d.find_element(By.ID, "admin-language-cb" + lang_id).click()
+        d.find_element(By.ID, "site-language-cb" + lang_id).click()
+        d.find_element(By.ID, "defaultLanguagesButton").click()
 
-        d.find_element_by_xpath(XPATHS["finish"]).click()
+        d.find_element(By.XPATH, XPATHS["finish"]).click()
 
         d.quit()
     except Exception as e:
-        if hasattr(e, "screen") and e.screen is not None:
-            with open("error.png", "wb") as f:
-                f.write(base64.decodebytes(e.screen.encode()))
-        else:
-            with open("error.html", "w") as f:
-                f.write(d.page_source)
+        d.save_screenshot("error.png")
+        with open("error.html", "w") as f:
+            f.write(d.page_source)
         raise
 
 

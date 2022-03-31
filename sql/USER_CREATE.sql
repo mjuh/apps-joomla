c6d325f386a3da010ec653fec15eec57b9535385
Зелинский Александр <zelinskiy@majordomo.ru>
2022-03-31 19:56:07 +0300

    documentation laid to me

--- sql/USER_CREATE.sql
+++ sql/USER_CREATE.sql
@@ -1,3 +1,3 @@
 INSERT INTO `${TABLE_PREFIX}_user_usergroup_map` (`user_id`, `group_id`) VALUES (360,8);
-INSERT INTO `${TABLE_PREFIX}_users` (`id`, `name`, `username`, `email`, `password`, `usertype`, `block`, `sendEmail`, `registerDate`, `lastvisitDate`, `activation`, `params`) VALUES
-(360,'${ADMIN_USERNAME}','${ADMIN_USERNAME}','${ADMIN_EMAIL}','${ADMIN_PASSWORD_HASH}','',0,1,'${INSTALL_DATETIME}','${INSTALL_DATETIME}','','');
+INSERT INTO `${TABLE_PREFIX}_users` (`id`, `name`, `username`, `email`, `password`, `sendEmail`, `registerDate`, `lastvisitDate`, `params`) VALUES
+(360,'${ADMIN_USERNAME}','${ADMIN_USERNAME}','${ADMIN_EMAIL}','${ADMIN_PASSWORD_HASH}',1,'${INSTALL_DATETIME}','${INSTALL_DATETIME}','');
